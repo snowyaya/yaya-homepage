25a907a967b71a4bae53456a131ef2f77b4d9557
snowyaya <yayingliang@gmail.com>
2022-02-04 03:34:26 -0500

    update works pge

--- pages/works.js
+++ pages/works.js
@@ -2,6 +2,7 @@ import { Container, Heading, SimpleGrid, Divider } from '@chakra-ui/react'
 import Layout from '../components/layouts/article'
 import Section from '../components/section'
 import { WorkGridItem } from '../components/grid-item'
+import { WorkImage } from '../components/work'
 
 const Works = () => (
   <Layout title="Works">
@@ -12,14 +13,10 @@ const Works = () => (
 
       <SimpleGrid columns={[1, 1, 2]} gap={6}>
         <Section delay={0.1}>
-          <WorkGridItem id="krypt" title="Krypt" thumbnail="/images/works/krypt.png">
-            Using web 3.0 to send your Crypto anywhere in the world.
-          </WorkGridItem>
+          <WorkImage src="/images/works/krypt.png" />
         </Section>
         <Section delay={0.1}>
-          <WorkGridItem id="cryptopunk" title="Crypto-Punk" thumbnail="/images/works/cryptopunk.png">
-            Various of crypto punks are displayed with a dynamic view.
-          </WorkGridItem>
+          <WorkImage src="/images/works/cryptopunk.png" />
         </Section>
         {/* <Section delay={0.2}>
           <WorkGridItem
@@ -32,13 +29,7 @@ const Works = () => (
         </Section> */}
 
         <Section delay={0.3}>
-          <WorkGridItem
-            id="complimentr"
-            title="Complimentr"
-            thumbnail="/images/works/complimentr.png"
-          >
-            A web app where you can send appreciating message to people who you are grateful for!
-          </WorkGridItem>
+          <WorkGridItem src="/images/works/complimentr.png" />
         </Section>
       </SimpleGrid>
 
@@ -51,23 +42,13 @@ const Works = () => (
 
       <SimpleGrid columns={[1, 1, 2]} gap={6}>
         <Section delay={0.5}>
-            <WorkGridItem 
-            id="coviddataanalysis" 
-            thumbnail="/images/works/coviddataanalysis.png" 
-            title="Covid Data Analysis"
-            >
+            <WorkImage src="/images/works/coviddataanalysis.png" />
               A data analysis implementation which explicitly shows the relation between the real estate 
               market and the covid-19 vaccination.
-            </WorkGridItem>
         </Section>
         <Section delay={0.6}>
-          <WorkGridItem
-            id="sharingapp"
-            title="Sharing App"
-            thumbnail="/images/works/sharingapp.png"
-          >
+          <WorkImage src="/images/works/sharingapp.png" />
             An Andriod app that help you keep tracking of the status of your items.
-          </WorkGridItem>
         </Section>
       </SimpleGrid>
     </Container>
